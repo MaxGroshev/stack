5e76db7dc734f9f717b8d99e2eaf7431d66a484b
Maxime_Groshev <groshevmaxim84@yandex.ru>
2022-11-01 00:31:57 +0300

    project separated into files

--- main.cpp
+++ main.cpp
@@ -4,10 +4,12 @@ int main()
 {
     struct my_stack box = {};
     int num_of_elem = 0;
-    stack_init(&box);
 
     printf("Please input amount of elements \n");
     scanf("%d", &num_of_elem);
+
+    stack_init(&box);
+
     for(int i = 0; i < num_of_elem; i++)
     {
         stack_push(&box, i);
@@ -23,63 +25,5 @@ int main()
     return 0;
 }
 
-//---------------------------------------------------------------------------------------------------------------------------------------------
 
-void stack_init(my_stack* box)
-{
-    box -> counter = 0;
-    box -> data = (int*)calloc(box -> counter , sizeof(int));
 
-    STACK_CHECK
-}
-
-//-------------------------------------------------------------------------------------------------------------------------------------------
-
-void stack_push(my_stack* box, int element)
-{
-    STACK_CHECK
-
-    box -> data = (int*) realloc(box -> data, (box -> counter + 1) * sizeof(int));
-    box -> data[box -> counter++] = element;
-
-    STACK_CHECK
-}
-
-//----------------------------------------------------------------------------------------------------------------------------------------------
-
-int stack_pop(my_stack* box)
-{
-    box -> counter--;
-
-    STACK_CHECK
-
-    return box -> data[box -> counter];
-}
-
-//----------------------------------------------------------------------------------------------------------------------------------------------
-
-int stack_check(my_stack* box)
-{
-    if(box -> data == NULL)
-    {
-        return ERR_NULL_DATA;
-    }
-
-    if(sizeof(box -> data[box -> counter - 1]) != sizeof(int))
-    {
-        return WRONG_INPUT;
-    }
-
-    if(box -> counter == -1)
-    {
-        return OUT_OF_STACK;
-    }
-    return STACK_IS_OKEY;
-}
-
-//-----------------------------------------------------------------------------------------------------------------------------------------------
-
-void stack_delete(my_stack* box)
-{
-    free(box -> data );
-}
