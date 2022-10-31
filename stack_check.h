3c9282fbc6a385398e77ea7d56408dfba3bf4c90
Maxime_Groshev <groshevmaxim84@yandex.ru>
2022-10-31 20:51:35 +0300

    started getting avoid of check func

--- stack_check.h
+++ stack_check.h
@@ -1,10 +1,13 @@
 #if defined(DEBUG)
 #define STACK_CHECK                                                                                       \
+        if(stack_check(box) != STACK_IS_OKEY)                                                             \
         {FILE* print_fail = fopen("stack.txt", "a");                                                      \
+                                                                                                          \
         fprintf(print_fail, "ERROR IN FILE %s, FUNCTION %s, LINE %d\n", __FILE__, __FUNCTION__, __LINE__);\
-        fprintf(print_fail, "%d\n",box -> data[box -> counter - 1]);                                                                                                   \
+        fprintf(print_fail,"Current cell: %ld. Meaning of element: %d\n", box -> counter, box -> data[box -> counter]);                                                                                                   \
         fprintf(stderr, "Error of running program, check stack.txt! \007\n");                             \
-        exit(1) ;                                          \
+                                                                                                          \
+        exit(1) ;                                                                                         \
         fclose(print_fail);                                                                               \
         }                                                                                                 \
 
