bc3ccd7d64874ded4cbb031f9825227787bf9667
Maxime_Groshev <groshevmaxim84@yandex.ru>
2022-11-01 17:44:25 +0300

    testing

--- stack_check.h
+++ stack_check.h
@@ -1,12 +1,13 @@
 #if defined(DEBUG)
 #define STACK_CHECK                                                                                       \
-        do{int fault = stack_check(box);                                                                  \
-        if(fault != STACK_IS_OKEY)                                                                        \
+        do{ int fault = stack_check(box);                                                                 \
+        if (fault != STACK_IS_OKEY)                                                                       \
         {                                                                                                 \
         FILE* print_fail = fopen("stack.txt", "a");                                                       \
                                                                                                           \
         fprintf(print_fail,"ERROR IN FILE %s, FUNCTION %s, LINE %d, CODE OF ERROR %d\n", __FILE__, __FUNCTION__, __LINE__, fault); \
-        fprintf(print_fail,"Current cell: %ld. Meaning of element: %d\n", box -> counter, box -> data[box -> counter]);\
+        fprintf(print_fail,"Current cell: %ld. [%d]\n", box -> counter, box -> data[box -> counter]);\
+        fprintf(print_fail,"Previous cell: %ld. Value of element: %d\n", box -> counter , box -> data[box -> counter]);\
         fprintf(stderr, "Error of running program, check stack.txt! \007\n");                             \
                                                                                                           \
         exit(1) ;                                                                                         \
@@ -18,6 +19,5 @@
 #define PRINT_FAIL ;
 
 #endif
-
 
 
