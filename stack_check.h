ebb9c76b8e3e238e3cf7af7e5de65295a8ed280c
Maxim_Groshev <112169730+MaximeGroshev@users.noreply.github.com>
2022-11-04 11:25:31 +0300

    some changes

--- stack_check.h
+++ stack_check.h
@@ -4,7 +4,7 @@
         if (stack_dump(box) != STACK_IS_OKEY)                                                                       \
         {                                                                                                 \
         FILE* print_fail = fopen("stack.txt", "a");                                                       \
-        fprintf(print_fail,"ERROR IN FILE %s, FUNCTION %s, LINE %d, CODE OF ERROR\n", __FILE__, __FUNCTION__, __LINE__); \
+        fprintf(print_fail,"ERROR IN FILE %s, FUNCTION %s, LINE %d\n", __FILE__, __FUNCTION__, __LINE__); \
         fprintf(stderr, "Error of running program, check stack.txt! \007\n");                             \
         fclose(print_fail);                                                                               \
         exit(1);                                                                                          \
